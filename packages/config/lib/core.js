f411d57cd56dc9496ae0e4c3401e60b6be50211a
Adriean Khisbe <adriean.khisbe@live.fr>
2019-02-05 15:26:51 +0100

    Retrieve original code from the progression API

--- packages/config/lib/core.js
+++ packages/config/lib/core.js
@@ -1,5 +1,20 @@
-const greeter = () => {
-  return 'Hello my new lib';
+'use strict';
+
+const merge = require('lodash/fp/merge');
+const getOr = require('lodash/fp/getOr');
+const coreConfig = require('./config');
+
+const configs = {
+  development: require('./development'),
+  production: require('./production'),
+  staging: require('./staging'),
+  test: require('./test')
 };
 
-module.exports = {greeter};
+function getEnvConfig(forceEnv) {
+  const env = forceEnv || getOr('development', 'NODE_ENV', process.env);
+  // eslint-disable-next-line import/no-dynamic-require
+  return merge(coreConfig, configs[env]());
+}
+
+module.exports = getEnvConfig;
