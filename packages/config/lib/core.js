7bb8b8d668c4e496e9cdbbef1604c719d3fec028
Adriean Khisbe <adriean.khisbe@live.fr>
2019-02-05 16:08:03 +0100

    Implement a fully functional getConfiguration fonction

--- packages/config/lib/core.js
+++ packages/config/lib/core.js
@@ -1,23 +1,29 @@
 const fs = require('fs');
 const path = require('path');
+const merge = require('lodash/fp/merge');
 
 function getConfiguration(configFolder, envName, envMapping) {
   // ensure existance of folder
   if (!fs.existsSync(configFolder)) {
-    throw new new Error(`unexpected config folder, given ${configFolder}`);
+    throw new Error(`unexpected config folder, given ${configFolder}`);
   }
-
-  const defaults = `${configFolder}/defaults.js`;
 
   // retrieve defaults config
-  if (!fs.existsSync(defaults)) {
-    throw Error(`can't retrieve defaults from ${configFolder}`);
+  if (!fs.existsSync(path.join(configFolder, 'defaults.js'))) {
+    throw new Error(`can't retrieve defaults from ${configFolder}`);
   }
+  // eslint-disable-next-line import/no-dynamic-require
+  const defaultsFactory = require(path.join(configFolder, 'defaults'));
+  const defaultConfig = defaultsFactory(envMapping);
 
-  // va fetcher le fichier envName dans config folder. (err -> crash)
-  // Injecte les envs pour avoir
-  // merge la config
-  return {bioutiful: 'config'};
+  if (!fs.existsSync(path.join(configFolder, `${envName}.js`))) {
+    throw new Error(`can't retrieve ${envName} config from ${configFolder}`);
+  }
+  // eslint-disable-next-line import/no-dynamic-require
+  const envFactory = require(path.join(configFolder, envName));
+  const envConfig = envFactory(envMapping);
+
+  return merge(defaultConfig, envConfig);
 }
 
 const resolveRelativePath = (folder, configPath) => path.join(folder, configPath);
