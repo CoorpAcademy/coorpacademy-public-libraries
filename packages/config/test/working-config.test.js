f01228f4a4893ed0999f73b7b85387609cb63e14
Julien Vaillant <julienvaillant@MacBookightness.localdomain>
2019-02-05 16:57:41 +0100

    fix linting issues

--- packages/config/test/working-config.test.js
+++ packages/config/test/working-config.test.js
@@ -5,25 +5,23 @@ const Config = require('..');
 const from = _path => Config.from(__dirname, _path);
 
 test('should get overriden value based on envName value', t => {
+  const conf = Config.get(from('configs/working-config'), 'staging', {});
 
-	const conf = Config.get(from('configs/working-config'), 'staging', {});
-
-	t.is(conf.number, 412)
-
+  t.is(conf.number, 412);
 });
 
 test('should rely on default without env override', t => {
-	const conf = Config.get(from('configs/working-config'), 'staging', {});
+  const conf = Config.get(from('configs/working-config'), 'staging', {});
 
-	t.is(conf.nodeEnv, 'development'); // no env given
+  t.is(conf.nodeEnv, 'development'); // no env given
 });
 
 test('should get configuration for some concrete example', t => {
-	const conf = Config.get(from('configs/working-config'), 'production', {NODE_ENV: 'production'});
+  const conf = Config.get(from('configs/working-config'), 'production', {NODE_ENV: 'production'});
 
-	t.deepEqual(conf, {
-	nodeEnv: 'production',
-  	number: 4012,
-  	chaine: 'the production string'
-	});
+  t.deepEqual(conf, {
+    nodeEnv: 'production',
+    number: 4012,
+    chaine: 'the production string'
+  });
 });
