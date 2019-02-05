4fec26db4c47b41d6e3cb92cec9b88c01ea58da7
Julien Vaillant <julienvaillant@MacBookightness.localdomain>
2019-02-05 15:45:41 +0100

    implement existence tests

--- packages/config/lib/core.js
+++ packages/config/lib/core.js
@@ -2,6 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 function getConfiguration(configFolder, envName, envMapping) {
+
+	if (! fs.existsSync(configFolder)) {
+    throw Error(`unexpected config folder, given ${configFolder}`)
+	}
+
+	const defaults = `${configFolder}/defaults`;
+
+	if (!fs.existsSync(defaults)) {
+		throw Error(`can't retrieve defaults from ${configFolder}`)
+	}
+  
   // vérification que le folder existe
   // va fetcher les defaults dans config folder. (err -> crash) (defaults)
   // va fetcher le fichier envName dans config folder. (err -> crash)
